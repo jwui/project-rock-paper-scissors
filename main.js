b6e79a64a20ccebb42259b8a4ad4496e1ed5fee5
snoopy2080 <49978401+snoopy2080@users.noreply.github.com>
2022-05-02 11:30:29 +0900

    Finished game logic

--- main.js
+++ main.js
@@ -4,33 +4,42 @@ function computerPlay() {
   return result;
 }
 
+var userScore = 0;
+var computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == "Rock" && computerSelection == "Scissors") {
     alert("You win! Rock beats Scissors!");
+    userScore++;
   }
   if (playerSelection == "Rock" && computerSelection == "Rock") {
     alert("Draw! Rocks can't beat one another!");
   }
   if (playerSelection == "Rock" && computerSelection == "Paper") {
     alert("You lose! Paper beats rock!");
+    computerScore++;
   }
   if (playerSelection == "Scissors" && computerSelection == "Paper") {
     alert("You win! Scissors beat Paper!");
+    userScore++;
   }
   if (playerSelection == "Scissors" && computerSelection == "Scissors") {
     alert("Draw! Scissors can't beat one another!");
   }
   if (playerSelection == "Scissors" && computerSelection == "Rock") {
     alert("You lose! Rock beats Scissors!");
+    computerScore++;
   }
   if (playerSelection == "Paper" && computerSelection == "Rock") {
     alert("You win! Paper beats Rock!");
+    userScore++;
   }
   if (playerSelection == "Paper" && computerSelection == "Paper") {
     alert("Draw! Papers can't beat one another!");
   }
   if (playerSelection == "Paper" && computerSelection == "Scissors") {
     alert("You lose! Scissor beats Paper!");
+    computerScore++;
   }
 }
 
@@ -38,4 +47,12 @@ for (let i = 0; i < 5; i++) {
   let userSelection = prompt("Please enter your choice");
   let computerSelection = computerPlay();
   playRound(userSelection, computerSelection);
+}
+
+if (userScore > computerScore) {
+  alert("You've won the game!");
+} else if (userScore == computerScore) {
+  alert("The game was a draw!");
+} else {
+  alert("You've lost the game!");
 }
