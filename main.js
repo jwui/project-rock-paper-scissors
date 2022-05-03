c0db47604159afa00db051eaa80d1586b05c6d38
snoopy2080 <49978401+snoopy2080@users.noreply.github.com>
2022-05-03 12:20:42 +0900

    Updated codes

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == "Rock" && computerSelection == "Scissors") {
-    alert("You win! Rock beats Scissors!");
+    alert("You win! rock beats scissors!");
     userScore++;
   }
   if (playerSelection == "Rock" && computerSelection == "Rock") {
@@ -20,39 +20,58 @@ function playRound(playerSelection, computerSelection) {
     computerScore++;
   }
   if (playerSelection == "Scissors" && computerSelection == "Paper") {
-    alert("You win! Scissors beat Paper!");
+    alert("You win! Scissors beat paper!");
     userScore++;
   }
   if (playerSelection == "Scissors" && computerSelection == "Scissors") {
     alert("Draw! Scissors can't beat one another!");
   }
   if (playerSelection == "Scissors" && computerSelection == "Rock") {
-    alert("You lose! Rock beats Scissors!");
+    alert("You lose! Rock beats scissors!");
     computerScore++;
   }
   if (playerSelection == "Paper" && computerSelection == "Rock") {
-    alert("You win! Paper beats Rock!");
+    alert("You win! Paper beats rock!");
     userScore++;
   }
   if (playerSelection == "Paper" && computerSelection == "Paper") {
     alert("Draw! Papers can't beat one another!");
   }
   if (playerSelection == "Paper" && computerSelection == "Scissors") {
-    alert("You lose! Scissor beats Paper!");
+    alert("You lose! Scissors beat paper!");
     computerScore++;
+  }
+  document.getElementById("userScore").innerHTML = userScore;
+  document.getElementById("computerScore").innerHTML = computerScore;
+  if (userScore == 5) {
+    alert("You've won the game! Press F5 to reset scores.");
+  } else if (computerScore == 5) {
+    alert("CPU has won the game! Press F5 to reset scores.");
   }
 }
 
+/*
 for (let i = 0; i < 5; i++) {
   let userSelection = prompt("Please enter your choice");
   let computerSelection = computerPlay();
   playRound(userSelection, computerSelection);
 }
+*/
 
-if (userScore > computerScore) {
-  alert("You've won the game!");
-} else if (userScore == computerScore) {
-  alert("The game was a draw!");
-} else {
-  alert("You've lost the game!");
-}
+var rock = document.getElementById("rock");
+var paper = document.getElementById("paper");
+var scissors = document.getElementById("scissors");
+
+rock.addEventListener("click", function () {
+  let computerSelection = computerPlay();
+  playRound("Rock", computerSelection);
+});
+paper.addEventListener("click", function () {
+  let computerSelection = computerPlay();
+  playRound("Paper", computerSelection);
+});
+
+scissors.addEventListener("click", function () {
+  let computerSelection = computerPlay();
+  playRound("Scissors", computerSelection);
+});
